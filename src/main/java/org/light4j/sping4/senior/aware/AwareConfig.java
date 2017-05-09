d08b7faa2bacf7ad9d7ff9dc40433ed114e4f182
longjiazuo <812198178@qq.com>
2017-05-09 21:54:03 +0800

    增加Spring Aware代码示例

---
+++ src/main/java/org/light4j/sping4/senior/aware/AwareConfig.java
@@ -0,0 +1,9 @@
+package org.light4j.sping4.senior.aware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan("org.light4j.sping4.senior.aware")
+public class AwareConfig {
+
+}
