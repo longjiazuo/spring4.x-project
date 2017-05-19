424d95d5c7b9d4efbd84b1b9ee062925d9c315ca
longjiazuo <812198178@qq.com>
2017-05-19 21:08:33 +0800

    增加Spring Profile的示例代码

---
+++ spring4.x-profile/src/main/java/org/light4j/sping4/usually/profile/DemoBean.java
@@ -0,0 +1,20 @@
+package org.light4j.sping4.usually.profile;
+
+public class DemoBean {
+
+	private String content;
+
+	public DemoBean(String content) {
+		super();
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
