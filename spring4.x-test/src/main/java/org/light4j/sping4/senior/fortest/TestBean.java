c40ee9dedb05bd3a40136b0afefdf230d89993b3
longjiazuo <812198178@qq.com>
2017-05-19 22:02:30 +0800

    增加Spring4测试的示例代码

---
+++ spring4.x-test/src/main/java/org/light4j/sping4/senior/fortest/TestBean.java
@@ -0,0 +1,19 @@
+package org.light4j.sping4.senior.fortest;
+
+public class TestBean {
+	private String content;
+
+	public TestBean(String content) {
+		super();
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
