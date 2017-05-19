592aec36ed6a3b9e0ac539c217eaeb5234badc1f
longjiazuo <812198178@qq.com>
2017-05-19 21:13:46 +0800

    增加Spring事件的示例代码

---
+++ spring4.x-event/src/main/java/org/light4j/sping4/usually/event/EventConfig.java
@@ -0,0 +1,10 @@
+package org.light4j.sping4.usually.event;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.light4j.sping4.usually.event")
+public class EventConfig {
+
+}
