8a3b95574dd0be6a9e937a3e1a916239c32ef996
longjiazuo <812198178@qq.com>
2017-05-19 20:51:15 +0800

    增加Bean的Scope示例代码

---
+++ spring4.x-scope/src/main/java/org/light4j/sping4/usually/scope/DemoSingletonService.java
@@ -0,0 +1,8 @@
+package org.light4j.sping4.usually.scope;
+
+import org.springframework.stereotype.Service;
+
+@Service //①
+public class DemoSingletonService {
+
+}
---
+++ spring4.x-scope/src/main/java/org/light4j/sping4/usually/scope/Main.java
@@ -0,0 +1,20 @@
+package org.light4j.sping4.usually.scope;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class); 
+		DemoSingletonService s1 = context.getBean(DemoSingletonService.class);
+        DemoSingletonService s2 = context.getBean(DemoSingletonService.class);
+
+        DemoPrototypeService p1 = context.getBean(DemoPrototypeService.class);
+        DemoPrototypeService p2 = context.getBean(DemoPrototypeService.class);
+
+        System.out.println("s1与s2是否相等："+s1.equals(s2));
+        System.out.println("p1与p2是否相等："+p1.equals(p2));
+        
+        context.close();
+	}
+}
